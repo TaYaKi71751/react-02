b681757d08ebe23d254f2f4f52e506d6a5f24519
TaYaKi71751 <shantian.tw@gmail.com>
2025-05-12 15:15:11 +0900

    Use index as 404

--- src/component/EmptyPage.js
+++ src/component/EmptyPage.js
@@ -2,6 +2,6 @@ import {Link} from "react-router-dom";
 export default function EmptyPage(){
     return(<>
         <h2> 잘못된 접근</h2>
-        <Link to={process.env.PUBLIC_URL}>돌아가기</Link>
+        <Link to="/">돌아가기</Link>
     </>)
 }
